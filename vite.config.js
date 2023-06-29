1d8166ae46d50f7e48c01fc071b6cc918d544f1d
Ravi Mori <devravimori@gmail.com>
2023-06-29 20:10:29 +0530

    added public path in config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  publicPath: process.env.NODE_ENV === "production" ? "https://devravimori.github.io/expense-tracker/" : "/",  
 })
